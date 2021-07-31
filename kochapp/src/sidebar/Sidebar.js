1b3fe9649023aeb24dba8274606f59d3a261e953
Jan Albert <j.albert@teckpro.de>
2021-07-31 23:03:30 +0200

    Refactoring Sidebar

--- kochapp/src/sidebar/Sidebar.js
+++ kochapp/src/sidebar/Sidebar.js
@@ -4,14 +4,16 @@ import React from "react"
 import {RenderRoutes} from "../routes/routes"
 import SidebarContent from "./SidebarContent"
 
-function Sidebar(props) {
+function Sidebar({routes}) {
     return <>
         <div className="dropdown">
             <button className='dropbtn'><FontAwesomeIcon icon={faBars} style={{fontSize: '20px'}}/></button>
-            <SidebarContent routes={props.routes} />
+            <SidebarContent routes={routes} />
         </div>
-        {/*Benoetigt fuer nested routing*/}
-        <div style={{padding: '1px 16px'}}>{RenderRoutes(props)}</div>
+        <div style={{padding: '1px 16px'}}>
+            {/*Benoetigt fuer nested routing*/}
+            <RenderRoutes routes={routes} />
+        </div>
     </>
 }
 
